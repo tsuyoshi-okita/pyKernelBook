d6bcab17c7d604f35779a1d241a43711553c86d3
tsuyoshi-okita <tsuyoshi.okita@gmail.com>
2016-10-18 12:32:52 +0200

    Add files via upload

--- pyKernelBook/__init__.py
+++ pyKernelBook/__init__.py
@@ -1,1 +1,47 @@
+# pyKernelBook
+#
+# Copyright (C) 2008 Tsuyoshi Okita
+# Authors: Tsuyoshi Okita <tsuyoshi_okita@yahoo.co.jp>
+# URL:     http://yahoo.co.jp/
+# For license information, see LICENSE.TXT
 
+"""
+pyKernelBook is a python packages implementing algorithm on 'Kernel Methods for Pattern Analysis' (Cambridge University Press, Japanese Edition: Kyoritsu publisher)
+@version: 0.1
+"""
+
+##//////////////////////////////////////////////////////
+##  Metadata
+##//////////////////////////////////////////////////////
+
+# Version.  For each new release, the version number should be updated
+# here and in the Epydoc comment (above).
+__version__ = "0.1"
+
+# Copyright notice
+__copyright__ = """\
+Copyright (C) 2008 Tsuyoshi Okita.
+
+Distributed and Licensed under provisions of the GNU Public
+License, which is included by reference.
+"""
+
+__license__ = "MIT License"
+# Description of the toolkit, keywords, and the project's primary URL.
+__longdescr__ = """\
+pyKernelBook is a Python package for
+processing natural language text.  NLTK requires Python 2.4 or higher."""
+__keywords__ = ['kernel methods', 'machine learning'] 
+__url__ = "http://github.com/tsuyoshi-okita"
+
+# Maintainer, contributors, etc.
+__maintainer__ = "Tsuyoshi Okita"
+__maintainer_email__ = "tsuyoshi_okita@yahoo.co.jp"
+__author__ = __maintainer__
+__author_email__ = __maintainer_email__
+
+# Import top-level functionality into top-level namespace
+
+#import data
+
+# Processing packages -- these all define __all__ carefully.
